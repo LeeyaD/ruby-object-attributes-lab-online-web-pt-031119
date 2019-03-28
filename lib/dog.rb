68557b8fced145515775bd6d291332352a43517e
Leeya Davis <leeya463@gmail.com>
2019-03-28 22:55:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,15 @@
+require 'pry'
 class Dog
 
-def name= (name)
-  @name = "name"
-end
+  def name= (name)
+    @name = "name"
+  end
 
-def name
-  @name
+  def name
+    @name
+  end
 end
-
+binding.pry
 fido = Dog.new
 fido.name= "Fido"
 fido.name
